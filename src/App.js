8cec1ed12103257d559c0e86f87181b1a144fb03
Aditya Pundir <68149659+Aditya-Pundir@users.noreply.github.com>
2021-05-22 23:05:02 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { DataFetcher } from "./DataFetcher";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <DataFetcher />
+    </div>
+  );
+}
+
+export default App;
